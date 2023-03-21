4af17a18bcb4cc81cf0605df70a761ed4508fcb1
QuiD-0 <wodnd101@naver.com>
2023-03-21 16:44:57 +0900

    redisTemplate 추가

--- src/main/java/com/quid/commerce/config/RedisConfig.java
+++ src/main/java/com/quid/commerce/config/RedisConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,6 +19,15 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisRepositoryFactory() {
         return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisRepositoryFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return redisTemplate;
     }
 
 }
