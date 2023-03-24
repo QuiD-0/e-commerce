864bde3cb11edf19d48aba28457b5076e0428f52
QuiD-0 <wodnd101@naver.com>
2023-03-24 10:23:26 +0900

    양방향 연관관계 설정

--- src/main/java/com/quid/commerce/product/domain/ProductGroup.java
+++ src/main/java/com/quid/commerce/product/domain/ProductGroup.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,6 +24,8 @@ public class ProductGroup {
     private Long id;
 
     private String groupCode;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Keyword keyword;
 
     @OneToMany(mappedBy = "productGroup", cascade = CascadeType.ALL)
     private List<Product> productList;
