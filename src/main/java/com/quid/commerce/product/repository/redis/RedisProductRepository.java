4a4147a614f9bfad907b89c1cf12a1d73942b7d7
QuiD-0 <wodnd101@naver.com>
2023-03-28 10:02:57 +0900

    caching

--- src/main/java/com/quid/commerce/product/repository/redis/RedisProductRepository.java
+++ src/main/java/com/quid/commerce/product/repository/redis/RedisProductRepository.java
@@ -2,6 +2,8 @@ package com.quid.commerce.product.repository.redis;
 
 import com.quid.commerce.component.RedisBase;
 import com.quid.commerce.product.domain.Product;
+import java.time.Duration;
+import java.util.Optional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -10,7 +12,16 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class RedisProductRepository {
 
+    private final static Duration CACHE_TTL = Duration.ofSeconds(10);
     private final RedisBase redisBase;
+
+    public void save(String key, Product value) {
+        redisBase.save(getKey(key), value, CACHE_TTL);
+    }
+
+    public Optional<Product> find(String key) {
+        return redisBase.getData(getKey(key), Product.class);
+    }
 
     public Set getZsetValue(String key) {
         return redisBase.getZsetData(key);
@@ -22,5 +33,9 @@ public class RedisProductRepository {
 
     public void deleteZsetValue(Product product) {
         redisBase.deleteZsetData(product.getGroupCode(), product.getProductId());
+    }
+
+    private static String getKey(String key) {
+        return "product::" + key;
     }
 }
