d138a6aa42561d7403ca6ae7efbeff75e84bd27f
QuiD-0 <wodnd101@naver.com>
2023-04-05 17:45:45 +0900

    주문 생성 request 추가

--- src/main/java/com/quid/commerce/order/controller/request/OrderCreateRequest.java
+++ src/main/java/com/quid/commerce/order/controller/request/OrderCreateRequest.java
@@ -1,5 +1,16 @@
 package com.quid.commerce.order.controller.request;
 
-public record OrderCreateRequest() {
+import com.quid.commerce.order.domain.OrdererInfo;
+import java.util.Map;
 
+public record OrderCreateRequest(Map<Long, Integer> productMap, OrdererInfo ordererInfo) {
+
+    public OrderCreateRequest {
+        if (productMap == null || productMap.isEmpty()) {
+            throw new IllegalArgumentException("상품이 존재하지 않습니다.");
+        }
+        if (ordererInfo == null) {
+            throw new IllegalArgumentException("주문자 정보가 존재하지 않습니다.");
+        }
+    }
 }
